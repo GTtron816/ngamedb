749f76c0709e24b1d1e6303817032a4530e89e10
Abin Alex Koshy <abinak322@gmail.com>
2022-12-09 22:49:51 +0530

    USER AUTHENTICATION ADDED

--- ngamedb/settings.py
+++ ngamedb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'newgames',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,10 +122,11 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR,"static"
-]
+STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL= 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- ngamedb/urls.py
+++ ngamedb/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('newgames.urls')),
+    path('users/',include('django.contrib.auth.urls')),
+    path('users/',include('users.urls')),
    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
